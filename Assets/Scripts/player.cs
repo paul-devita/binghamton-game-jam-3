c55447af52d7540109b3dcb7c3cbc371e42ec5db
BuildTools <unconfigured@null.spigotmc.org>
2024-11-08 19:01:38 -0500

    added basic player movement script

--- Assets/Scripts/player.cs
+++ Assets/Scripts/player.cs
@@ -1,9 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class player : MonoBehaviour
 {
+    private const float GROUNDCHECK_RADIUS = 0.2f;
+    
     private float _horizontalInput;
     private float _speed = 8f;
     private float _jumpingPower = 16f;
@@ -23,9 +26,31 @@ public class player : MonoBehaviour
     
     void Update()
     {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _jumpingPower);
+        }
+
+        if (Input.GetButtonUp("Jump") && _rigidbody2D.velocity.y > 0f)
+        {
+            _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * 0.5f);
+        }
         
+        changeDirection();
     }
 
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(_horizontalInput * _speed, _rigidbody2D.velocity.y);
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.OverlapCircle(_groundCheck.position, GROUNDCHECK_RADIUS, _groundLayer);
+    }
+    
     private void changeDirection()
     {
         if (_isFacingRight && _horizontalInput < 0f || !_isFacingRight && _horizontalInput > 0f)
